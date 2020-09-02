7cdbf5853c710ffbe15c3c455396999752c1b075
Feverfew826 <lightkill826@gmail.com>
2020-09-02 17:00:45 +0900

    Basic puzzle created.
    puzzle init.
    puzzle move.
    puzzle shuffle.
    puzzle size.
    realtime puzzle shuffle.
    
    ToDo:
    complete check.
    new game button.
    image selection.
    image user input.
    define puzzle size and ratio and original image ratio.
    advanced shuffle.

---
+++ Assets/Scripts/PuzzlePiece.cs
@@ -0,0 +1,267 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePiece : MonoBehaviour
+{
+    public abstract class Dependency
+    {
+        public abstract Vector2 GetTileSize();
+    }
+    public Dependency dependency;
+
+    private class InterpolatedMovement
+    {
+        private Transform transform;
+        public Vector3 startPosition { get; private set; }
+        public Vector3 targetPosition { get; private set; }
+        public float movementTime { get; private set; }
+        public float passedTime { get; private set; }
+        public bool shouldMove { get; private set; }
+
+        public InterpolatedMovement(Transform transform)
+        {
+            this.transform = transform;
+            startPosition = transform.position;
+            targetPosition = startPosition;
+            movementTime = 0.001f;
+            passedTime = 0;
+            shouldMove = false;
+        }
+
+        public void SetMovement(Vector3 targetPosition, float time, bool skipPrevious = true)
+        {
+            if (time < 0.001f)
+                time = 0.001f;
+
+            this.movementTime = time;
+            passedTime = 0;
+
+            if (skipPrevious)
+                transform.position = this.targetPosition;
+
+            startPosition = transform.position;
+            this.targetPosition = targetPosition;
+
+            shouldMove = true;
+        }
+        public void SetRelativeMovement(Vector3 distance, float time, bool skipPrevious = true)
+        {
+            if (skipPrevious)
+                SetMovement(targetPosition + distance, time, true);
+            else
+                SetMovement(transform.position + distance, time, false);
+        }
+        public void UpdateMovement(float deltaTime)
+        {
+            if (shouldMove)
+            {
+                passedTime += deltaTime;
+                transform.position = Vector3.Lerp(startPosition, targetPosition, passedTime / movementTime);
+
+                if (transform.position == targetPosition)
+                    shouldMove = false;
+            }
+        }
+        public void ForceToFinishMovement()
+        {
+            shouldMove = false;
+            transform.position = targetPosition;
+        }
+    }
+    private InterpolatedMovement movement;
+
+    public PuzzlePiece top;
+    public PuzzlePiece bottom;
+    public PuzzlePiece left;
+    public PuzzlePiece right;
+
+    private IEnumerator moveCoroutine;
+    private Vector3 startPosition;
+    private Vector3 targetPosition;
+
+    public bool isEmptyPiece = false;
+    void Awake()
+    {
+        targetPosition = transform.position;
+        movement = new InterpolatedMovement(transform);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.UpdateMovement(Time.deltaTime);
+    }
+    
+    public void OnMouseDown()
+    {
+        if (top != null && top.isEmptyPiece)
+        {
+            AimToTop(dependency.GetTileSize());
+        }
+        else if (bottom != null && bottom.isEmptyPiece)
+        {
+            AimToBottom(dependency.GetTileSize());
+        }
+        else if (left != null && left.isEmptyPiece)
+        {
+            AimToLeft(dependency.GetTileSize());
+        }
+        else if (right != null && right.isEmptyPiece)
+        {
+            AimToRight(dependency.GetTileSize());
+        }
+    }
+
+    void AimToTop(Vector2 tileSize)
+    {
+        Debug.Log("Up");
+
+        Vector3 distance = new Vector3(0, tileSize.y, 0);
+        movement.SetRelativeMovement(distance, 0.1f, true);
+
+        PuzzlePiece topBackup = top;
+        PuzzlePiece bottomBackup = bottom;
+        PuzzlePiece leftBackup = left;
+        PuzzlePiece rightBackup = right;
+
+        if (bottomBackup)
+            bottomBackup.top = topBackup;
+        if (leftBackup)
+            leftBackup.right = topBackup;
+        if (rightBackup)
+            rightBackup.left = topBackup;
+
+        if (topBackup.top)
+            topBackup.top.bottom = this;
+        if (topBackup.left)
+            topBackup.left.right = this;
+        if (topBackup.right)
+            topBackup.right.left = this;
+
+        top = topBackup.top;
+        bottom = topBackup;
+        left = topBackup.left;
+        right = topBackup.right;
+
+        topBackup.top = this;
+        topBackup.bottom = bottomBackup;
+        topBackup.left = leftBackup;
+        topBackup.right = rightBackup;
+    }
+    void AimToBottom(Vector2 tileSize)
+    {
+        Debug.Log("Bottom");
+
+        Vector3 distance = new Vector3(0, -tileSize.y, 0);
+        movement.SetRelativeMovement(distance, 0.1f, true);
+
+        PuzzlePiece topBackup = top;
+        PuzzlePiece bottomBackup = bottom;
+        PuzzlePiece leftBackup = left;
+        PuzzlePiece rightBackup = right;
+
+        if (topBackup)
+            topBackup.bottom = bottomBackup;
+        if (leftBackup)
+            leftBackup.right = bottomBackup;
+        if (rightBackup)
+            rightBackup.left = bottomBackup;
+
+        if (bottomBackup.bottom)
+            bottomBackup.bottom.top = this;
+        if (bottomBackup.left)
+            bottomBackup.left.right = this;
+        if (bottomBackup.right)
+            bottomBackup.right.left = this;
+
+        top = bottomBackup;
+        bottom = bottomBackup.bottom;
+        left = bottomBackup.left;
+        right = bottomBackup.right;
+
+        bottomBackup.top = topBackup;
+        bottomBackup.bottom = this;
+        bottomBackup.left = leftBackup;
+        bottomBackup.right = rightBackup;
+    }
+    void AimToLeft(Vector2 tileSize)
+    {
+        Debug.Log("Left");
+
+        Vector3 distance = new Vector3(-tileSize.x, 0, 0);
+        movement.SetRelativeMovement(distance, 0.1f, true);
+
+        PuzzlePiece topBackup = top;
+        PuzzlePiece bottomBackup = bottom;
+        PuzzlePiece leftBackup = left;
+        PuzzlePiece rightBackup = right;
+
+        if (topBackup)
+            topBackup.bottom = leftBackup;
+        if (bottomBackup)
+            bottomBackup.top = leftBackup;
+        if (rightBackup)
+            rightBackup.left = leftBackup;
+
+        if (leftBackup.top)
+            leftBackup.top.bottom = this;
+        if (leftBackup.bottom)
+            leftBackup.bottom.top = this;
+        if (leftBackup.left)
+            leftBackup.left.right = this;
+
+        top = leftBackup.top;
+        bottom = leftBackup.bottom;
+        left = leftBackup.left;
+        right = leftBackup;
+
+        leftBackup.top = topBackup;
+        leftBackup.bottom = bottomBackup;
+        leftBackup.left = this;
+        leftBackup.right = rightBackup;
+    }
+    void AimToRight(Vector2 tileSize)
+    {
+        Debug.Log("Right");
+
+        Vector3 distance = new Vector3(tileSize.x, 0, 0);
+        movement.SetRelativeMovement(distance, 0.1f, true);
+
+        PuzzlePiece topBackup = top;
+        PuzzlePiece bottomBackup = bottom;
+        PuzzlePiece leftBackup = left;
+        PuzzlePiece rightBackup = right;
+
+        if (topBackup)
+            topBackup.bottom = rightBackup;
+        if (bottomBackup)
+            bottomBackup.top = rightBackup;
+        if (leftBackup)
+            leftBackup.right = rightBackup;
+
+        if (rightBackup.top)
+            rightBackup.top.bottom = this;
+        if (rightBackup.bottom)
+            rightBackup.bottom.top = this;
+        if (rightBackup.right)
+            rightBackup.right.left = this;
+
+        top = rightBackup.top;
+        bottom = rightBackup.bottom;
+        left = rightBackup;
+        right = rightBackup.right;
+
+        rightBackup.top = topBackup;
+        rightBackup.bottom = bottomBackup;
+        rightBackup.left = leftBackup;
+        rightBackup.right = this;
+    }
+
+}
---
+++ Assets/Scripts/SlidePuzzleGameMode.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D.Path;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SlidePuzzleGameMode : MonoBehaviour
+{
+    public int puzzleDiagonalNum;
+    public float puzzleSize;
+    public Texture2D imageTexture;
+    public SpriteRenderer preview;
+    public GameObject puzzlePrefab;
+    public GameObject emptyPuzzlePrefab;
+    public int shuffleTime;
+    public bool shouldShuffleRealTime;
+    public bool fastRealTimeShffle;
+
+    private class PieceDependency : PuzzlePiece.Dependency
+    {
+        public Vector2 tileSize;
+        public override Vector2 GetTileSize()
+        {
+            return tileSize;
+        }
+    }
+    private PieceDependency pieceDependency;
+
+    private struct TileCreationUnit
+    {
+        public Rect rect;
+        public PuzzlePiece piece;
+        public SpriteRenderer renderer;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (puzzleDiagonalNum < 2)
+            puzzleDiagonalNum = 2;
+
+        float tileImageWidth = imageTexture.width / puzzleDiagonalNum;
+        float tileImageHeight = imageTexture.height / puzzleDiagonalNum;
+        Vector2 tileImageSize = new Vector2(tileImageWidth, tileImageHeight);
+
+        Vector2 ratio = new Vector2();
+        ratio.x = Math.Min(1, tileImageWidth / tileImageHeight);
+        ratio.y = Math.Min(1, tileImageHeight / tileImageWidth);
+
+        if (puzzleSize < 1)
+            puzzleSize = 1;
+
+        Vector2 tileSize = new Vector2();
+        tileSize.x = puzzleSize / puzzleDiagonalNum * ratio.x;
+        tileSize.y = puzzleSize / puzzleDiagonalNum * ratio.y;
+
+        pieceDependency = new PieceDependency();
+        pieceDependency.tileSize = tileSize;
+
+        Quaternion tileRotation = Quaternion.identity;
+
+        PuzzlePiece emptyPiece = null;
+        List<TileCreationUnit> creationUnits = new List<TileCreationUnit>();
+        Dictionary<KeyValuePair<int, int>, PuzzlePiece> pieceMap = new Dictionary<KeyValuePair<int, int>, PuzzlePiece>();
+        for (int x = 0; x < puzzleDiagonalNum; x++)
+        {
+            for (int y = 0; y < puzzleDiagonalNum; y++)
+            {
+                TileCreationUnit unit = new TileCreationUnit();
+                if (x == 0 && y == 0)
+                {
+                    Vector3 tilePosition = new Vector3();
+                    GameObject newObject = Instantiate<GameObject>(emptyPuzzlePrefab, tilePosition, tileRotation);
+
+                    emptyPiece = newObject.GetComponent<PuzzlePiece>();
+                    emptyPiece.isEmptyPiece = true;
+                    unit.piece = emptyPiece;
+                }
+                else
+                {
+                    float xPadding = tileImageWidth * x;
+                    float yPadding = tileImageHeight * y;
+                    Vector2 tilePadding = new Vector2(xPadding, yPadding);
+                    unit.rect = new Rect(tilePadding, tileImageSize);
+
+                    Vector3 tilePosition = new Vector3();
+                    tilePosition.x = tileSize.x * x;
+                    tilePosition.y = tileSize.y * y;
+                    GameObject newObject = Instantiate<GameObject>(puzzlePrefab, tilePosition, tileRotation);
+                    newObject.GetComponent<BoxCollider2D>().size = tileSize;
+
+                    unit.piece = newObject.GetComponent<PuzzlePiece>();
+                    unit.renderer = newObject.GetComponent<SpriteRenderer>();
+
+                    creationUnits.Add(unit);
+                }
+
+                unit.piece.dependency = pieceDependency;
+
+                KeyValuePair<int, int> idx = new KeyValuePair<int, int>(x, y);
+                pieceMap.Add(idx, unit.piece);
+            }
+        }
+        Rect previewImageRect = new Rect(0, 0, imageTexture.width, imageTexture.height);
+        preview.sprite = Sprite.Create(imageTexture, previewImageRect, new Vector2(.5f, .5f));
+
+        SetSprite(creationUnits, tileImageSize, tileSize);
+        ConnectEachPieces(pieceMap);
+
+        if(shouldShuffleRealTime)
+            StartCoroutine(RealtimeShuffle(emptyPiece, shuffleTime));
+        else
+            Shuffle(emptyPiece, shuffleTime);
+
+        Debug.Log("EndOfStart");
+    }
+
+    void SetSprite(List<TileCreationUnit> creationUnits, Vector2 tileImageSize, Vector2 tileSize)
+    {
+        
+        float pixelsPerUnit = Math.Max(tileImageSize.x / tileSize.x, tileImageSize.y / tileSize.y);
+        foreach (var unit in creationUnits)
+        {
+            Vector2 pivot = new Vector2(.5f, .5f);
+            Sprite newSprite = Sprite.Create(imageTexture, unit.rect, pivot, pixelsPerUnit);
+
+            unit.renderer.sprite = newSprite;
+        }
+    }
+
+    void ConnectEachPieces(Dictionary<KeyValuePair<int, int>, PuzzlePiece> pieceMap)
+    {
+        for (int x = 0; x < puzzleDiagonalNum; x++)
+        {
+            for (int y = 0; y < puzzleDiagonalNum; y++)
+            {
+                KeyValuePair<int, int> idx = new KeyValuePair<int, int>(x, y);
+
+                KeyValuePair<int, int> topIdx = new KeyValuePair<int, int>(x, y + 1);
+                KeyValuePair<int, int> bottomIdx = new KeyValuePair<int, int>(x, y - 1);
+                KeyValuePair<int, int> leftIdx = new KeyValuePair<int, int>(x - 1, y);
+                KeyValuePair<int, int> rightIdx = new KeyValuePair<int, int>(x + 1, y);
+
+                if (pieceMap.ContainsKey(topIdx))
+                    pieceMap[idx].top = pieceMap[topIdx];
+                if (pieceMap.ContainsKey(bottomIdx))
+                    pieceMap[idx].bottom = pieceMap[bottomIdx];
+                if (pieceMap.ContainsKey(leftIdx))
+                    pieceMap[idx].left = pieceMap[leftIdx];
+                if (pieceMap.ContainsKey(rightIdx))
+                    pieceMap[idx].right = pieceMap[rightIdx];
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Shuffle(PuzzlePiece emptyPiece, int times)
+    {
+        PuzzlePiece usedPiece = null;
+        for(int i = 0; i < times; i++)
+        {
+            usedPiece = RandomMovement(emptyPiece, usedPiece);
+        }
+    }
+
+    IEnumerator RealtimeShuffle(PuzzlePiece emptyPiece, int times)
+    {
+        PuzzlePiece usedPiece = null;
+        for (int i = 0; i < times; i++)
+        {
+            if(fastRealTimeShffle)
+                yield return new WaitForSeconds(0.2f);
+            else
+                yield return new WaitForSeconds(1.0f);
+
+            usedPiece = RandomMovement(emptyPiece, usedPiece);
+        }
+    }
+
+    PuzzlePiece RandomMovement(PuzzlePiece emptyPiece, PuzzlePiece exceptPiece)
+    {
+        List<PuzzlePiece> selectablePieces = new List<PuzzlePiece>();
+        if (emptyPiece.top && emptyPiece.top != exceptPiece)
+            selectablePieces.Add(emptyPiece.top);
+        if (emptyPiece.bottom && emptyPiece.bottom != exceptPiece)
+            selectablePieces.Add(emptyPiece.bottom);
+        if (emptyPiece.left && emptyPiece.left != exceptPiece)
+            selectablePieces.Add(emptyPiece.left);
+        if (emptyPiece.right && emptyPiece.right != exceptPiece)
+            selectablePieces.Add(emptyPiece.right);
+
+        System.Random random = new System.Random();
+        int randomIdx = random.Next(selectablePieces.Count);
+        Debug.Log(selectablePieces.Count);
+        PuzzlePiece selectedPiece = selectablePieces[randomIdx];
+
+        selectedPiece.OnMouseDown();
+
+        return selectedPiece;
+    }
+
+    void StartNewPuzzle()
+    {
+        Debug.Log("Delete All puzzles. create new one. set new sprite. reorder connectivity. check destruction.");
+    }
+}
